354700c54a2316f350d59578e115be5cccdfa3cd
adriendavin <adriendavin@hotmail.fr>
2023-04-17 12:15:47 +0200

    css mods
    Signed-off-by: adriendavin <adriendavin@hotmail.fr>

---
+++ src/components/Die.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import React from "react";
+
+export default function Die(props) {
+  return (
+    <div className="die--face">
+      <h2 className="die--num">{props.value}</h2>
+    </div>
+  )
+}
