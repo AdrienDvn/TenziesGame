8c477153d923c33524946766fae01f4c91a7d370
adriendavin <adriendavin@hotmail.fr>
2023-04-17 11:22:30 +0200

    css base mods ok
    Signed-off-by: adriendavin <adriendavin@hotmail.fr>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <main className="main">
-      <div className='body'>
-        <h2 className='body--title'>
-           Tenzies
-         </h2>
-      </div>
+    <main>
+      hello
     </main>
   );
 }
-
-export default App;
