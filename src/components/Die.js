a29de933ebad8cf67084a353b21e976aa4742edf
adriendavin <adriendavin@hotmail.fr>
2023-04-18 09:33:26 +0200

    mods
    Signed-off-by: adriendavin <adriendavin@hotmail.fr>

--- src/components/Die.js
+++ src/components/Die.js
@@ -2,6 +2,18 @@ import { useState } from "react";
 import React from "react";
 
 export default function Die(props) {
+
+  function allNewDice() {
+    const newDice =[];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.floor(Math.random() * 6) + 1)
+    }
+    return (
+      newDice
+    )
+  }
+
+
   return (
     <div className="die--face">
       <h2 className="die--num">{props.value}</h2>
